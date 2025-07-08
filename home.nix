b00c7f454ba8f042d225ca9820fce3cef43f6001
Szabo Istvan <pisti4395@gmail.com>
2025-07-08 20:50:11 +0200

    asdf

--- home.nix
+++ home.nix
@@ -1,19 +1,19 @@
 {options, config, pkgs, lib, ... }:
-  with lib;
-  let 
+with lib;
+let 
   mod = "Mod4";
-	 nixvim = import (builtins.fetchGit {
-        url = "https://github.com/nix-community/nixvim";
-        # When using a different channel you can use `ref = "nixos-<version>"` to set it here
-    });
-   #nur = import (builtins.fetchTarball {
-   # url = "https://github.com/nix-community/NUR/archive/main.tar.gz";
-    # pin with sha256 for less frequent downloads
+  nixvim = import (builtins.fetchGit {
+    url = "https://github.com/nix-community/nixvim";
+    # When using a different channel you can use `ref = "nixos-<version>"` to set it here
+  });
+  #nur = import (builtins.fetchTarball {
+  # url = "https://github.com/nix-community/NUR/archive/main.tar.gz";
+  # pin with sha256 for less frequent downloads
   #}) { inherit pkgs; };
-  in {
+in {
   #nixpkgs.overlays = [ nur.overlay ];
   #sway config
-	imports = [nixvim.homeManagerModules.nixvim];
+  imports = [nixvim.homeManagerModules.nixvim];
   wayland.windowManager.sway = {
     enable = true;
     config = rec {
@@ -24,7 +24,7 @@
       bars = [];
       input."*".xkb_layout = "us";
       keybindings = lib.attrsets.mergeAttrsList  [
-      #(lib.attrsets.mergeAttrsList)
+        #(lib.attrsets.mergeAttrsList)
         {
           "${mod}+Return" = "exec --no-startup-id alacritty";
           "${mod}+p" = "exec --no-startup-id wofi --show drun";
@@ -49,25 +49,25 @@
       output = {
         eDP-1 = {
           scale = "1";
-	        position = "0 0";
-	      };
-	      HDMI-A-1 = {
-	        scale = "1";
-	        position = "1920 0";
-	      };
+          position = "0 0";
+        };
+        HDMI-A-1 = {
+          scale = "1";
+          position = "1920 0";
+        };
       };
       workspaceOutputAssign = [
-      { workspace = "6"; output = "HDMI-A-1"; }
-      { workspace = "7"; output = "HDMI-A-1"; }
-      { workspace = "8"; output = "HDMI-A-1"; }
-      { workspace = "9"; output = "HDMI-A-1"; }
-      { workspace = "10"; output = "HDMI-A-1"; }
+        { workspace = "6"; output = "HDMI-A-1"; }
+        { workspace = "7"; output = "HDMI-A-1"; }
+        { workspace = "8"; output = "HDMI-A-1"; }
+        { workspace = "9"; output = "HDMI-A-1"; }
+        { workspace = "10"; output = "HDMI-A-1"; }
 
-      { workspace = "1"; output = "eDP-1"; }
-      { workspace = "2"; output = "eDP-1"; }
-      { workspace = "3"; output = "eDP-1"; }
-      { workspace = "4"; output = "eDP-1"; }
-      { workspace = "5"; output = "eDP-1"; }
+        { workspace = "1"; output = "eDP-1"; }
+        { workspace = "2"; output = "eDP-1"; }
+        { workspace = "3"; output = "eDP-1"; }
+        { workspace = "4"; output = "eDP-1"; }
+        { workspace = "5"; output = "eDP-1"; }
       ];
     };
     extraConfig = "workspace 1 output eDP-1\nworkspace 2 output eDP-1\nworkspace 6 output HDMI-A-1\nworkspace 7 output HDMI-A-1";
@@ -109,18 +109,18 @@
     profiles = {
       default = {
         id = 0;
-	name = "Default";
-	isDefault = true;
-	#settings = {
-	#  "browser.startup.homepage" = "https://duckduckgo.com";
-	#};
-	search = {
-	  force = true;
-	  default = "ddg";
+        name = "Default";
+        isDefault = true;
+        #settings = {
+        #  "browser.startup.homepage" = "https://duckduckgo.com";
+        #};
+        search = {
+          force = true;
+          default = "ddg";
         };
-	extensions.packages = with pkgs.nur.repos.rycee.firefox-addons; [
-	  ublock-origin vimium darkreader sponsorblock
-	];
+        extensions.packages = with pkgs.nur.repos.rycee.firefox-addons; [
+          ublock-origin vimium darkreader sponsorblock
+        ];
       };
     };
   };
@@ -148,228 +148,177 @@
     '';
   };
 
-programs.nixvim = {
-  imports = [
-    /home/istipisti113/.config/home-manager/nixvim
-  ];
-  enable = true;
-  defaultEditor = true;
-  viAlias = true;
-  vimAlias = true;
-  opts = {
-    number = true;
-    relativenumber = true;
-    shiftwidth = 4;
-    expandtab = true;
-    smartindent = true;
-    undofile = true;
-    termguicolors = true;
-    signcolumn = "yes";
-    scrolloff = 8;
-  };
-
-  keymaps = [
-    {
-      key = "{";
-      action = "}";
-    }
-    {
-      key = "}";
-      action = "{";
-    }
-  ];
-  colorschemes.tokyonight.enable = true;
-
-  plugins = {
-    treesitter = {
-      enable = true;
-      settings.indent.enable = true;
-      settings.highlight.enable = true;
+  programs.nixvim = {
+    imports = [
+      /home/istipisti113/.config/home-manager/nixvim
+    ];
+    enable = true;
+    highlight.ExtraWhitespace.bg = "red";
+    defaultEditor = true;
+    viAlias = true;
+    vimAlias = true;
+    opts = {
+      number = true;
+      relativenumber = true;
+      shiftwidth = 2;
+      expandtab = true;
+      smartindent = true;
+      undofile = true;
+      termguicolors = true;
+      signcolumn = "yes";
+      scrolloff = 8;
     };
-    cmp.enable = true;
-    lualine.enable = true;
-    lsp.servers = {
-      rust_analyzer = {
-	enable = true;
-	autostart = true;
-	installCargo = true;
-	installRustc = true;
+
+    keymaps = [
+      {
+        key = "{";
+        action = "}";
+      }
+      {
+        key = "}";
+        action = "{";
+      }
+    ];
+    colorschemes.tokyonight.enable = false;
+    colorschemes.catppuccin.enable = true;
+
+    plugins = {
+      treesitter = {
+        enable = true;
+        settings.indent.enable = true;
+        settings.highlight.enable = true;
       };
-      lua_ls.enable = true;
-      lua_ls.autostart = true;
-      nixd.enable = true;
-      nixd.autostart = true;
-    };
-    lint = {
-      enable = true;
-      lintersByFt = {
-	lua = ["luacheck"];
-	nix = ["nix"];
-        rust = ["rust-analyzer"];
+      cmp.enable = true;
+      lualine.enable = true;
+      lsp.servers = {
+        rust_analyzer = {
+          enable = true;
+          autostart = true;
+          installCargo = true;
+          installRustc = true;
+        };
+        lua_ls.enable = true;
+        lua_ls.autostart = true;
+        nixd.enable = true;
+        nixd.autostart = true;
+      };
+      lint = {
+        enable = true;
+        lintersByFt = {
+          lua = ["luacheck"];
+          nix = ["nix"];
+          rust = ["rust-analyzer"];
+        };
       };
     };
-  };
 
-  extraPlugins = with pkgs.vimPlugins; [
-    nvim-lspconfig
-  ];
-  extraConfigLua = ''
+    extraPlugins = with pkgs.vimPlugins; [
+      nvim-lspconfig
+    ];
+    extraConfigLua = ''
   local function set_cmn_lsp_keybinds()
-      	local lsp_keybinds = {
-      		{
-      			key = "K",
-      			action = vim.lsp.buf.hover,
-      			options = {
-      				buffer = 0,
-      				desc = "hover [K]noledge with LSP",
-      			},
-      		},
-      		{
-      			key = "gd",
-      			action = vim.lsp.buf.definition,
-      			options = {
-      				buffer = 0,
-      				desc = "[g]o to [d]efinition with LSP",
-      			},
-      		},
-      		{
-      			key = "gy",
-      			action = vim.lsp.buf.type_definition,
-      			options = {
-      				buffer = 0,
-      				desc = "[g]o to t[y]pe definition with LSP",
-      			},
-      		},
-      		{
-      			key = "gi",
-      			action = vim.lsp.buf.implementation,
-      			options = {
-      				buffer = 0,
-      				desc = "[g]o to [i]mplementation with LSP",
-      			},
-      		},
-      		{
-      			key = "<leader>dj",
-      			action = vim.diagnostic.goto_next,
-      			options = {
-      				buffer = 0,
-      				desc = "Go to next [d]iagnostic with LSP",
-      			},
-      		},
-      		{
-      			key = "<leader>dk",
-      			action = vim.diagnostic.goto_prev,
-      			options = {
-      				buffer = 0,
-      				desc = "Go to previous [d]iagnostic with LSP",
-      			},
-      		},
-      		{
-      			key = "<leader>r",
-      			action = vim.lsp.buf.rename,
-      			options = {
-      				buffer = 0,
-      				desc = "[r]ename variable with LSP",
-      			},
-      		},
-      	}
+    local lsp_keybinds = {
+      {
+        key = "K",
+        action = vim.lsp.buf.hover,
+        options = {
+          buffer = 0,
+          desc = "hover [K]noledge with LSP",
+        },
+      },
+      {
+        key = "gd",
+        action = vim.lsp.buf.definition,
+        options = {
+          buffer = 0,
+          desc = "[g]o to [d]efinition with LSP",
+        },
+      },
+      {
+        key = "gy",
+        action = vim.lsp.buf.type_definition,
+        options = {
+          buffer = 0,
+          desc = "[g]o to t[y]pe definition with LSP",
+        },
+      },
+      {
+        key = "gi",
+        action = vim.lsp.buf.implementation,
+        options = {
+          buffer = 0,
+          desc = "[g]o to [i]mplementation with LSP",
+        },
+      },
+      {
+        key = "<leader>dj",
+        action = vim.diagnostic.goto_next,
+        options = {
+          buffer = 0,
+          desc = "Go to next [d]iagnostic with LSP",
+        },
+      },
+      {
+        key = "<leader>dk",
+        action = vim.diagnostic.goto_prev,
+        options = {
+          buffer = 0,
+          desc = "Go to previous [d]iagnostic with LSP",
+        },
+      },
+      {
+        key = "<leader>r",
+        action = vim.lsp.buf.rename,
+        options = {
+          buffer = 0,
+          desc = "[r]ename variable with LSP",
+        },
+      },
+    }
 
-      	for _, bind in ipairs(lsp_keybinds) do
-      		vim.keymap.set("n", bind.key, bind.action, bind.options)
-      	end
-      end
-    -- Nix LSP
-      require("lspconfig").nixd.setup({
-      	on_attach = function()
-      		set_cmn_lsp_keybinds()
-      	end,
-      	settings = {
-      		nixd = {
-      			formatting = {
-      				command = { "nixfmt" },
-      			},
-      		},
-      	},
-      })
-    -- Rust LSP
-      require("lspconfig").rust_analyzer.setup({
-      	root_dir = function(fname)
-      		return vim.loop.cwd()
-      	end,
-      	settings = {
-      		['rust-analyzer'] = {
-      			cargo = {
-      				allFeatures = true,
-      			},
-      		},
-      	},
-      	on_attach = function()
-      		set_cmn_lsp_keybinds()
-      	end,
-      })
+    for _, bind in ipairs(lsp_keybinds) do
+      vim.keymap.set("n", bind.key, bind.action, bind.options)
+    end
+  end
+  -- Nix LSP
+  require("lspconfig").nixd.setup({
+    on_attach = function()
+      set_cmn_lsp_keybinds()
+    end,
+    settings = {
+      nixd = {
+        formatting = {
+          command = { "nixfmt" },
+        },
+      },
+    },
+  })
+  -- Rust LSP
+  require("lspconfig").rust_analyzer.setup({
+    root_dir = function(fname)
+      return vim.loop.cwd()
+    end,
+    settings = {
+      ['rust-analyzer'] = {
+        cargo = {
+          allFeatures = true,
+        },
+      },
+    },
+    on_attach = function()
+      set_cmn_lsp_keybinds()
+    end,
+  })
   '';
-};
+  };
 
-programs.neovim = {
-  enable = true;
-  extraPackages = with pkgs; [rust-analyzer telescope ripgrep fd ];
-  plugins = with pkgs.vimPlugins; [
-    nvim-lspconfig
-    nvim-cmp cmp-nvim-lsp cmp-buffer cmp-path cmp_luasnip nvim-lspconfig
-    lazy-nvim
-  ];
- # extraLuaConfig = ''
- # 	vim.g.mapleader = " "
- #   vim.opt.tabstop = 2
- #   vim.opt.shiftwidth = 2
- #   vim.wo.number = true
- #   vim.wo.relativenumber = true
- # 	vim.keymap.set('n', '{', '}', {noremap = true})
- # 	vim.keymap.set('n', '}', '{', {noremap = true})
- # 	vim.keymap.set('n', 'j', 'gj', {noremap = true})
- # 	vim.keymap.set('n', 'k', 'gk', {noremap = true})
- # 	vim.api.nvim_set_keymap('n', '<leader>d', '<cmd>lua vim.diagnostic.open_float()<CR>', {noremap = true, silent=true})
- # 	--vim.api.nvim_set_keymap('n', '<leader>dd', '<cmd>Telescope diagnostics<CR>', { noremap = true, silent = true })
- #   require('lspconfig').rust_analyzer.setup{
- #   }  
- # 	vim.cmd([[filetype plugin indent on]])
- # 	vim.cmd([[syntax enable]])
- # 	require("lazy").setup({
- # 		"neovim/nvim-lspconfig",
- # 		{
- # 		  "nvim-treesitter/nvim-treesitter",
- # 			prefer_git = true;
- # 		  build = ":TSUpdate",
- # 		  ensure_installed = { "lua", "python", "c", "rust", "dart", "nix"},
- # 		  parser_install_dir = "/home/istipisti113/treesitterParsers",
- # 		  config = function()
- # 		    require("nvim-treesitter.configs").setup({
- # 		      parser_install_dir = "/home/istipisti113/treesitterParserss",
- # 		      ensure_installed = { "lua", "python", "c", "rust", "dart", "nix"},
- # 		      sync_install = false,
- # 		      auto_install = true,
- # 		      highlight = {
- # 		        enable = true,
- # 		        additional_vim_regex_highlighting = true,
- # 		      },
- # 		    })
- # 		  end,
- # 		  lazy = false,  -- load immediately
- # 		},
- # 		{
- # 			'nvim-flutter/flutter-tools.nvim',
- #   		lazy = false,
- #   		dependencies = {
- #   		    'nvim-lua/plenary.nvim',
- #   		    'stevearc/dressing.nvim', -- optional for vim.ui.select
- #   		},
- #   		config = true,
- # 		},
-
- # 	})
- # 	require("flutter-tools").setup{}
- # '';
-};
-# ln -sf /home/istipisti113/.config/nvim/init.lua /root/.config/nvim/init.lua
-# setting the config for root with symlink
+  programs.neovim = {
+    enable = true;
+    extraPackages = with pkgs; [rust-analyzer telescope ripgrep fd ];
+    plugins = with pkgs.vimPlugins; [
+      nvim-lspconfig
+      nvim-cmp cmp-nvim-lsp cmp-buffer cmp-path cmp_luasnip nvim-lspconfig
+      lazy-nvim
+    ];
+  };
 }
